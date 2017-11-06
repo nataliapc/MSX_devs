198993af0173b683a7732390c3d7697eb763ce00
Natalia Pujol <npujol@gfi.es>
2017-11-06 15:46:38 +0100

    cas2tsx.php first submit

---
+++ TSXphpclass/cas2tsx.php
@@ -0,0 +1,29 @@
+<?php
+	include_once "tsx.php";
+	include_once "cas.php";
+
+	if ($argc<3) {
+		echo "\nSyntax:\n  php cas2tsx.php <filename.CAS> <filename.TSX>\n\n";
+		exit;
+	}
+	if (!file_exists($argv[1])) {
+		echo "\nFile not found: '$argv[1]'\n\n";
+		exit;
+	}
+
+	$cas = new CAS($argv[1]);
+	$tsx = new TSX();
+
+	for ($i=0; $i<$cas->getNumBlocks(); $i++) {
+		$bcas = $cas->getBlock($i);
+		echo "Block ".($i+1).": ".strlen($bcas->data())." bytes"."\n";
+		
+		$btsx = new Block4B();
+		$btsx->pause(1000);
+		$btsx->data($bcas->data());
+		$tsx->addBlock($btsx);
+	}
+
+	$tsx->saveToFile($argv[2]);
+
+?>

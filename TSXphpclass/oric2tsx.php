b1aca16efccd9b2aa9c803ad2272bad51a6e3c57
nataliapc <natalia.pujol@gmail.com>
2019-12-08 12:04:42 +0100

    Indentation changes

--- TSXphpclass/oric2tsx.php
+++ TSXphpclass/oric2tsx.php
@@ -16,9 +16,9 @@
 
 	//$tap = file_get_contents($argv[1]);
 
-  $fp = fopen($argv[1], 'r');
+	$fp = fopen($argv[1], 'r');
 	if (!$fp) {
-    echo 'Input file not found.';
+		echo 'Input file not found.';
 	}
 
 	$data="";
@@ -27,73 +27,65 @@
 
 	$tsx = new TSX();
 
-    while (!feof($fp)) {
-      if (ord(fgetc($fp))==0x16) {
-       do {
-          $valin=ord(fgetc($fp));
-          if (feof($fp)) break;
-        } while ($valin!=0x24);
-				echo ("Oric sync found...\n");
-        emit_sync();
+	while (!feof($fp)) {
+		if (ord(fgetc($fp))==0x16) {
+			do {
+				$valin=ord(fgetc($fp));
+				if (feof($fp)) break;
+			} while ($valin!=0x24);
+			echo ("Oric sync found...\n");
+			emit_sync();
 
-     for ($i=0;$i<9;$i++)
-		     {
-            $header[$i]=ord(fgetc($fp));
-						emit_byte($header[$i]);
+	 		for ($i=0;$i<9;$i++) {
+				$header[$i]=ord(fgetc($fp));
+				emit_byte($header[$i]);
 			}
-		 do {
-        $i=ord(fgetc($fp));
+			do {
+				$i=ord(fgetc($fp));
 				emit_byte($i);   /* name */
-      } while (($i!=0)&&(!feof($fp)));
+			} while (($i!=0)&&(!feof($fp)));
 
-     emit_gap();
+			emit_gap();
 
+			$size=($header[4]*256+$header[5])-($header[6]*256+$header[7])+1;
 
-		 $size=($header[4]*256+$header[5])-($header[6]*256+$header[7])+1;
+			echo("Found Block....... Size: ".$size."\n");
+			for ($i=0;$i<$size;$i++)
+				emit_byte(ord(fgetc($fp)));
+			foreach (range(0, strlen($bin_data), 8) as $j) {
+				$byte="";
+				$last_bits="";
+				for ($bit=0;$bit <=7;$bit++) {
+					if (isset($bin_data[$j+$bit]))
+					{
+						$byte=$byte.$bin_data[$j+$bit];
+						$last_bits=$last_bits."1";
+					} else {
+						$byte=$byte."0";
+						$last_bits=$last_bits."0";
+					}
+ 	 			}
+				$data=$data.chr(bindec($byte));
+			}
+			//Create the new data block
+			$b = new Block14();
+			//Define the 14  data block with Oric params
+			$b->zeroLen(1270) ; //1270
+			$b->oneLen(635) ; //635
+			$b->lastBits(bindec($last_bits)) ;
+			$b->pause(1000) ;
+			$b->data($data);
+			//Insert the new data block
+			$tsx->addBlock($b);
 
-		 echo("Found Block....... Size: ".$size."\n");
-     for ($i=0;$i<$size;$i++)
-			  emit_byte(ord(fgetc($fp)));
-				foreach (range(0, strlen($bin_data), 8) as $j)
-	 	     {
-	 	 			$byte="";
-	 	 			$last_bits="";
-	 	 			for ($bit=0;$bit <=7;$bit++)
-	 	 			{
-	 	 				if (isset($bin_data[$j+$bit]))
-	 	 				{
-	 	 				  $byte=$byte.$bin_data[$j+$bit];
-	 	 					$last_bits=$last_bits."1";
-	 	 				}
-	 	 			  else {
-	 	 			  	$byte=$byte."0";
-	 	 					$last_bits=$last_bits."0";
-	 	 				  }
+			echo("-------------------------\n");
 
-	 	 			}
-	 	 			$data=$data.chr(bindec($byte));
-	 	 		}
-				//Create the new data block
-				$b = new Block14();
-				//Define the 14  data block with Oric params
-				$b->zeroLen(1270) ; //1270
-				$b->oneLen(635) ; //635
-				$b->lastBits(bindec($last_bits)) ;
-				$b->pause(1000) ;
-				$b->data($data);
-				//Insert the new data block
-				$tsx->addBlock($b);
-
-				echo("-------------------------\n");
-
-				$data="";
-				$bin_data="";
-
-
-   }
+			$data="";
+			$bin_data="";
+		}
 	}
 	$tsx->saveToFile($argv[2]);
-  fclose($fp);
+	fclose($fp);
 
 
 
@@ -106,25 +98,23 @@ function emit_level($bit)
 
 function emit_bit($bit)
 {
-        if ($bit) {
-          emit_level(1);
-        } else {
-          emit_level(0);
-        }
-
+	if ($bit) {
+		emit_level(1);
+	} else {
+		emit_level(0);
+	}
 }
 
 
-
 function emit_byte($in) {
-  $out = 0;
-  $parity=1;
+	$out = 0;
+	$parity=1;
 	emit_bit(0);
-  for ($i=0; $i<8; $i++,$in>>=1) {
-       $parity=$parity+$in&1;
-       emit_bit($in & 1);
-       }
-  emit_bit($parity);
+	for ($i=0; $i<8; $i++,$in>>=1) {
+		$parity=$parity+$in&1;
+		emit_bit($in & 1);
+	}
+	emit_bit($parity);
 	emit_bit(1);
 	emit_bit(1);
 	emit_bit(1);
@@ -136,16 +126,16 @@ function emit_byte($in) {
 
 function emit_sync()
 {
-for ($i=0;$i<256;$i++)
-  emit_byte(0x16);
+	for ($i=0;$i<256;$i++)
+  		emit_byte(0x16);
 
-emit_byte(0x24);
+	emit_byte(0x24);
 }
 
 function emit_gap()
 {
-  for ($i=0;$i<100;$i++)
-	 emit_bit(1);
+	for ($i=0;$i<100;$i++)
+		emit_bit(1);
 }
 
 ?>
